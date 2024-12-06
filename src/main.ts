d252abf363fb2a2e152631b7b64989c716559bb5
adnanalvi68 <adnanalvi68@gmail.com>
2024-12-06 12:20:13 +0500

    date picker issue fixed

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,6 @@
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { appConfig } from './app/app.config';
-// import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch((err) => console.error(err));
-import { bootstrapApplication } from "@angular/platform-browser";
-import { provideHttpClient, HTTP_INTERCEPTORS } from "@angular/common/http";
-import { AppComponent } from "./app/app.component";
-import { authInterceptor } from "./app/interceptors/auth.interceptor";
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';  // Import NoopAnimationsModule
-import { provideAnimations } from '@angular/platform-browser/animations'; // Import provideAnimations for Angular 18
-
-// Bootstrap the application and provide HTTP interceptors
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),  // Configure HTTP client
-    {
-      provide: HTTP_INTERCEPTORS,
-      useValue: authInterceptor,
-      multi: true
-    },
-    provideAnimations()  // Use this instead of importing NoopAnimationsModule in `imports`
-  ]
-})
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));

8d8c4525be0c48d16477b7b35926795445fea688
adnanalvi68 <adnanalvi68@gmail.com>
2024-12-09 13:12:07 +0500

    token adding to interceptor

--- src/app/interceptors/auth.service.ts
+++ src/app/interceptors/auth.service.ts
@@ -26,7 +26,7 @@ export class AuthService {
     }
 
     get accessToken(): string {
-        return localStorage.getItem('accessToken') ?? '';
+        return localStorage.getItem('accessToken') ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoyMDQ4NzM3NDA3LCJpYXQiOjE3MzMzNzc0MDcsImp0aSI6ImFiM2NlZDZlNTI4MzRlMTdhMjcyOGIzZjY2ZDU4ZjJlIiwidXNlcl9pZCI6MX0.SbZhv67nD5T68FvsercJOrWPje98fppXK22AozfKitc';
     }
 
     // -----------------------------------------------------------------------------------------------------

64712590ff989512d8e92b93edb3f9b6b25fd358
rozzu <rozzujomsat@gmail.com>
2024-12-26 14:32:20 +0500

    Milestones feedback fixes

--- src/app/components/library/map-calendar/map-calendar.component.ts
+++ src/app/components/library/map-calendar/map-calendar.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import dayjs from "dayjs";
 import { NgxDaterangepickerMd } from "ngx-daterangepicker-material";
 import { SharedService } from "../../shared/shared.service";
+import { SatelliteService } from "../../../services/satellite.service";
 
 @Component({
   selector: "app-map-calendar",
@@ -10,13 +11,29 @@ import { SharedService } from "../../shared/shared.service";
   templateUrl: "./map-calendar.component.html",
   styleUrl: "./map-calendar.component.scss",
 })
-export class MapCalendarComponent {
+export class MapCalendarComponent implements OnInit {
   startDate: dayjs.Dayjs = dayjs(); // Initialize with current date
   endDate: dayjs.Dayjs = dayjs().add(0, "days"); // Initialize with 1 day later
   maxDate: dayjs.Dayjs = dayjs();
+  @Input() polygon_wkt:any
+  constructor(private sharedService:SharedService,
+    private satelliteService:SatelliteService
+  ){}
 
-  constructor(private sharedService:SharedService){}
-
+  ngOnInit(): void {
+    console.log(this.polygon_wkt,'inputting polygoninputting polygoninputting polygoninputting polygon');
+    if(this.polygon_wkt){
+      const payload = {
+        polygon_wkt: this.polygon_wkt
+      }
+      this.satelliteService.getPolygonCalenderDays(payload).subscribe({
+        next: (resp) => {
+          console.log(resp,'getPolygonCalenderDaysgetPolygonCalenderDaysgetPolygonCalenderDays');
+          
+        }})
+    }
+    
+  }
   choosedDate(event: any) {
     console.log("Selected Date and Time Range:", event);
   }

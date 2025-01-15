59263d336ab81943e635974ce678343b019471cc
rozzu <rozzujomsat@gmail.com>
2025-01-15 20:05:36 +0500

    Pop up library checkbox fixes

--- src/app/components/library/library.component.ts
+++ src/app/components/library/library.component.ts
@@ -1097,6 +1097,9 @@ roundOff(value: number): number {
   return Math.round(value);
 }
 
+toDecimal(value:number){
+  return parseFloat(value.toFixed(2));
+}
 // On checkbox change
 onCheckboxChange(row: any) {
   console.log(row,'imageHoverViewimageHoverViewimageHoverViewimageHoverViewimageHoverView');

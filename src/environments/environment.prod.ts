9d742739b52114990549748170a6898be3479240
rozzu <rozzujomsat@gmail.com>
2025-01-16 14:09:50 +0500

    socket added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     API_BASE_URL: "http://54.164.76.150:8000/api",
-    SOCKET_URL: "",
+    SOCKET_URL: "ws://54.164.76.150:8000/ws/messaging/global/",
     debugMode: false,
   };

83e76e57c2d5627aa19eac556f8ce0de67e2996e
rozzu <rozzujomsat@gmail.com>
2025-02-27 19:23:17 +0500

    Analysis chart tab view fixes

--- src/app/components/analysis/analysis.component.ts
+++ src/app/components/analysis/analysis.component.ts
@@ -47,9 +47,6 @@ export class AnalysisComponent {
         zoom: {
           enabled: true,
           type: 'xy'
-        },
-        toolbar: {
-          show: false 
         }
       },
       xaxis: {

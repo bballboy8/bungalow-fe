9654bb29e09b0c0bc45878e82d82fa42d05e69f8
rozzu <rozzujomsat@gmail.com>
2025-03-09 20:46:10 +0500

    fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [{
-    path: '', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
+    path: 'map', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
 },
 ];
